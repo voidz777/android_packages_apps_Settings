15231ad120a6b7a7dfd6b8dde71614243efc795a
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-11-24 15:36:29 +0000

    Profiles: fix pager strip color

--- src/com/android/settings/profiles/ProfilesSettings.java
+++ src/com/android/settings/profiles/ProfilesSettings.java
@@ -107,7 +107,7 @@ public class ProfilesSettings extends SettingsPreferenceFragment {
         mViewPager.setAdapter(mAdapter);
 
         PagerTabStrip tabs = (PagerTabStrip) view.findViewById(R.id.tabs);
-        tabs.setTabIndicatorColorResource(android.R.color.holo_blue_light);
+        tabs.setTabIndicatorColorResource(R.color.theme_accent);
 
         mProfileManager = (ProfileManager) getActivity().getSystemService(PROFILE_SERVICE);
 
