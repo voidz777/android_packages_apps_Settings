d320768339f9ca546ca9a3be6e1b3c2949de6240
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-15 08:13:35 +0000

    QS: add back Screen timeout tile
    Change-Id: I7cbdfbfc264e0fc7c5700c71e5e42bf7499cf51d

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -136,10 +136,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_visualizer_static";
                 stringId = R.string.qs_tile_visualizer;
                 break;
-            /**case QSConstants.TILE_SCREEN_TIMEOUT:
+            case QSConstants.TILE_SCREEN_TIMEOUT:
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
-                break;**/
+                break;
             case QSConstants.TILE_SCREENSHOT:
                 resourceName = "ic_qs_screenshot";
                 stringId = R.string.qs_screenshot_tile;
