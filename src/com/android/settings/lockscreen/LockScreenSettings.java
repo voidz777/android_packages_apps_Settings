ab5b9f0987e12f51f2340ee5acc404ab20077166
rohan <purohit.rohan@gmail.com>
2015-08-23 05:49:19 +0200

    Revert "SecuritySettings: Lockscreen wallpaper"
    This reverts commit b59283edb39a220d48e21af1191bbd3435525a76.
    
    Change-Id: I9bd85d337fa33170fbda19b84cd7fc1f34def4ce

--- src/com/android/settings/lockscreen/LockScreenSettings.java
+++ src/com/android/settings/lockscreen/LockScreenSettings.java
@@ -21,7 +21,6 @@ import android.app.ActivityManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -30,7 +29,6 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
@@ -78,8 +76,6 @@ public class LockScreenSettings extends SettingsPreferenceFragment
     private static final String KEY_MANAGE_TRUST_AGENTS = "manage_trust_agents";
     private static final String KEY_SHOW_VISUALIZER = "lockscreen_visualizer";
 
-    private static final String KEY_LOCKSCREEN_WALLPAPER = "lockscreen_wallpaper";
-
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_LIVELINESS_OFF = 125;
@@ -120,21 +116,6 @@ public class LockScreenSettings extends SettingsPreferenceFragment
                 && savedInstanceState.containsKey(TRUST_AGENT_CLICK_INTENT)) {
             mTrustAgentClickIntent = savedInstanceState.getParcelable(TRUST_AGENT_CLICK_INTENT);
         }
-
-        // Lockscreen wallpaper
-        PreferenceCategory generalCategory =
-                (PreferenceCategory) findPreference(KEY_GENERAL_CATEGORY);
-        if (generalCategory != null) {
-            PreferenceScreen lockscreenWallpaper = (PreferenceScreen)
-                generalCategory.findPreference(KEY_LOCKSCREEN_WALLPAPER);
-            if (lockscreenWallpaper != null) {
-                try {
-                    getActivity().getPackageManager().getPackageInfo("com.slim.wallpaperpicker", 0);
-                } catch (PackageManager.NameNotFoundException e) {
-                    generalCategory.removePreference(lockscreenWallpaper);
-                }
-            }
-         }
     }
 
     @Override
