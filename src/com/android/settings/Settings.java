5fa9819994068e1856cc92283f9e6c42c19a9714
ghsr <ghsr92@yandex.ru>
2015-09-03 15:11:12 +0600

    Settings: add changelog to about phone
    Change-Id: Ic8fea6441b601953828a4c6d75e3db377a70819c

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -113,5 +113,6 @@ public class Settings extends SettingsActivity {
     public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DisplayRotationActivity extends SettingsActivity { /* empty */ }
+    public static class ChangeLog extends SettingsActivity { /* empty */ }
 }
 
